4404f4a2580f91bea8d7d2acdf1b9d5f813c2363
WNJStudio <super@wnjstudio.com>
2025-09-18 10:33:19 +0200

    sync settings in localstorage

--- src/context/SettingsContext.svelte
+++ src/context/SettingsContext.svelte
@@ -1,25 +1,60 @@
 <script module>
     export const PIPEVIEW = "pipeline";
     export const DATAVIEW = "data";
+
+    const settings = JSON.parse(localStorage.getItem("UDP_SETTINGS") || "{}");
     /**
      * @type {PIPEVIEW|DATAVIEW}
      */
-    let currentView = $state(PIPEVIEW);
-    let isSidebarOpen = $state(true);
-    let isSnapToGrid = $state(true);
-    let gridSize = $state(20);
+    let currentView = $state(settings.currentView || PIPEVIEW);
+    let isSidebarOpen = $state(
+        settings.isSidebarOpen !== undefined ? settings.isSidebarOpen : true,
+    );
+    let isSnapToGrid = $state(
+        settings.isSnapToGrid !== undefined ? settings.isSnapToGrid : true,
+    );
+    let gridSize = $state(settings.gridSize || 20);
+
+    let lastChange = $state(Date.now());
+    let delayCheck = $state();
+
+    const delayedSave = () => {
+        lastChange = Date.now();
+        if (delayCheck) {
+            clearTimeout(delayCheck);
+            delayCheck = undefined;
+        }
+        delayCheck = setTimeout(() => {
+            if (Date.now() - lastChange > 500) {
+                localStorage.setItem(
+                    "UDP_SETTINGS",
+                    JSON.stringify({
+                        currentView,
+                        isSidebarOpen,
+                        isSnapToGrid,
+                        gridSize,
+                    }),
+                );
+            }
+        }, 500);
+    };
 
     /**
      * @param {PIPEVIEW|DATAVIEW} view
      */
-    const changeView = (view) => (currentView = view);
+    const changeView = (view) => {
+        currentView = view;
+        delayedSave();
+    };
 
     const toggleSidebar = () => {
         isSidebarOpen = !isSidebarOpen;
+        delayedSave();
     };
 
     const toggleSnapToGrid = () => {
         isSnapToGrid = !isSnapToGrid;
+        delayedSave();
     };
 
     /**
@@ -27,6 +62,7 @@
      */
     const changeGridSize = (size) => {
         gridSize = Math.min(200, Math.max(1, size));
+        delayedSave();
     };
 
     export const getViewChanger = () => changeView;
